2e77222cc83d9cbd61f99f3060d2d378b2f9b384
Yohanes Irwan Rinaldy <yohanesirwan.rinaldy@izeno.com>
2025-12-08 04:59:42 +0700

    initial commit

--- bamboo-specs/src/main/java/com/example/pnm/go/PublishAll.java
+++ bamboo-specs/src/main/java/com/example/pnm/go/PublishAll.java
@@ -1,9 +1,6 @@
 package com.example.pnm.go;
 
-import com.atlassian.bamboo.specs.api.builders.deployment.Deployment;
-import com.atlassian.bamboo.specs.api.builders.plan.Plan;
 import com.atlassian.bamboo.specs.util.BambooServer;
-import com.atlassian.bamboo.specs.util.SimpleCredentials;
 import com.example.pnm.go.deployment.Deployment_Go_App1;
 import com.example.pnm.go.deployment.Deployment_Go_App2;
 import com.example.pnm.go.deployment.Deployment_Go_App3;
@@ -20,39 +17,16 @@ import com.example.pnm.go.pipeline.Plan_Go_App3;
  */
 public class PublishAll {
     public static void main(String[] args) throws Exception {
-        String host = requiredEnv("BAMBOO_HOST");
-        String user = requiredEnv("BAMBOO_USER");
-        String pass = requiredEnv("BAMBOO_PASS");
+        BambooServer server = new BambooServer("http://18.143.196.145:8085");
 
-        BambooServer server = new BambooServer(host);
-        server.setCredentials(new SimpleCredentials(user, pass));
+        server.publish(new Plan_Go_App1().plan());
+        server.publish(new Plan_Go_App2().plan());
+        server.publish(new Plan_Go_App3().plan());
 
-        // Build plans
-        Plan plan1 = new Plan_Go_App1().plan();
-        Plan plan2 = new Plan_Go_App2().plan();
-        Plan plan3 = new Plan_Go_App3().plan();
+        server.publish(new Deployment_Go_App1().deploymentProject());
+        server.publish(new Deployment_Go_App2().deploymentProject());
+        server.publish(new Deployment_Go_App3().deploymentProject());
 
-        // Deployment projects
-        Deployment dep1 = new Deployment_Go_App1().deployment();
-        Deployment dep2 = new Deployment_Go_App2().deployment();
-        Deployment dep3 = new Deployment_Go_App3().deployment();
-
-        server.publish(plan1);
-        server.publish(plan2);
-        server.publish(plan3);
-
-        server.publish(dep1);
-        server.publish(dep2);
-        server.publish(dep3);
-
-        System.out.println("Published plans GOA1/2/3 and deployments GOA1/2/3 to Bamboo at " + host);
-    }
-
-    private static String requiredEnv(String key) {
-        String val = System.getenv(key);
-        if (val == null || val.isBlank()) {
-            throw new IllegalArgumentException("Missing env var: " + key);
-        }
-        return val;
+        System.out.println("Published GO plans & deployments to Bamboo.");
     }
 }
